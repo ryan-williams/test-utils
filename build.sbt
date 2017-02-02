f817499bb0903b46cb6e3042db239033c6b50a3d
Ryan Williams <ryan.blake.williams@gmail.com>
2017-02-02 16:37:07 +0000

    Conversions, CanEquals

--- build.sbt
+++ build.sbt
@@ -1,5 +1,5 @@
 name := "test-utils"
-version := "1.1.4"
+version := "1.1.5-SNAPSHOT"
 
 addScala212
 
--- src/main/scala/org/hammerlab/test/Suite.scala
+++ src/main/scala/org/hammerlab/test/Suite.scala
@@ -1,11 +1,33 @@
 package org.hammerlab.test
 
-import org.scalactic.ConversionCheckedTripleEquals
-import org.scalatest.{ FunSuite, Matchers }
+import java.{lang ⇒ jl}
+
+import org.hammerlab.test.files.TmpFiles
+import org.scalactic.{ CanEqual, ConversionCheckedTripleEquals }
+import org.scalatest.{ BeforeAndAfter, BeforeAndAfterAll, FunSuite, Matchers }
 
 // Simple wrapper for common test-suite boilerplate.
 class Suite
   extends FunSuite
     with Matchers
     with ConversionCheckedTripleEquals
-    with implicits.Templates
+    with BeforeAndAfterAll
+    with BeforeAndAfter
+    with TmpFiles {
+
+  def obviousEquality[L, R]: CanEqual[L, R] =
+    new CanEqual[L, R] {
+      override def areEqual(a: L, b: R): Boolean = a == b
+    }
+
+  // Some implicits to allow trivial conversions for type-safe equality checking with ===.
+  implicit val intLongEqual = obviousEquality[Int, Long]
+  implicit val longIntEqual = obviousEquality[Long, Int]
+  implicit val jlongIntEqual = obviousEquality[jl.Long, Int]
+  implicit val jlongLongEqual = obviousEquality[jl.Long, Long]
+  implicit val longJLongEqual = obviousEquality[Long, jl.Long]
+  implicit val integerIntEqual = obviousEquality[Integer, Int]
+  implicit val jboolBoolEqual = obviousEquality[jl.Boolean, Boolean]
+  implicit val doubleFloatEqual = obviousEquality[jl.Double, Float]
+  implicit val jfloatFloatEqual = obviousEquality[jl.Float, Float]
+}
